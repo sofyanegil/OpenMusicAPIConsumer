05a6fface50f55cbbc62f7f7ebb9c393c193f2f6
Sofyan Egi <sofyanegil@gmail.com>
2022-07-29 17:34:31 +0700

    feat: create MailSender

---
+++ src/MailSender.js
@@ -0,0 +1,33 @@
+const nodemailer = require('nodemailer');
+
+class MailSender {
+  constructor() {
+    this._transporter = nodemailer.createTransport({
+      host: process.env.MAIL_HOST,
+      port: process.env.MAIL_PORT,
+      auth: {
+        email: process.env.MAIL_ADDRESS,
+        pass: process.env.MAIL_PASSWORD,
+      },
+    });
+  }
+
+  sendEmail(targetEmail, content) {
+    const message = {
+      from: 'Notes Apps',
+      to: targetEmail,
+      subject: 'Ekspor Lagu dari Playlist OpenMusic',
+      text: 'Terlampir hasil dari ekspor lagu dari Playlist OpenMusic',
+      attachments: [
+        {
+          filename: 'playlist.json',
+          content,
+        },
+      ],
+    };
+
+    return this._transporter.sendMail(message);
+  }
+}
+
+module.exports = MailSender;
